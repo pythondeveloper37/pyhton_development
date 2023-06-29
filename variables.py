66472b430de4559c776c6b4acccf9fedaf3d3c93
Naveen kakumanu <kakumanunaveen111@gmail.com>
2023-06-29 08:12:47 +0530

    Helloworld and Variables Program

---
+++ variables.py
@@ -0,0 +1,16 @@
+# variables Program
+# variables Declaration
+name ="python class"
+count = 10 
+average = 5.6 
+
+#  Printing Variables
+
+print(name)
+print(count)
+print(average)
+
+# printing variables type
+print(type(name))
+print(type(count))
+print(type(average))
